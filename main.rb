d1cce87b516fde9e98fce9773355466825f95120
Osman Alhassan <oalhassan847@gmail.com>
2023-06-22 14:47:07 +0000

    Add message decoder

--- main.rb
+++ main.rb
@@ -45,3 +45,7 @@ end
 def decode_word(morse_word)
   morse_word.split.map { |char| decode_char(char) }.join
 end
+
+def decode(morse_message)
+  morse_message.split('   ').map { |word| decode_word(word) }.join(' ')
+end
